c5b40f908a4df54c11b9575eb1b89cfb0bdcc11b
Jonathan Kingston <jonathan@jooped.co.uk>
2012-04-27 15:37:15 +0100

    Collections - Hide away to only run on the server

--- stellar/stellar.js
+++ stellar/stellar.js
@@ -17,10 +17,12 @@ Stellar.setPage = function(controller, action) {
 
 Stellar.Collection = function(name) {
   collection = new Meteor.Collection(name);
-  Meteor.startup(function () {
-    _.each(['insert', 'update', 'remove'], function(method) {
-      Meteor.default_server.method_handlers['/' + name + '/' + method] = function() {};
+  if(Meteor.is_server) {
+    Meteor.startup(function () {
+      _.each(['insert', 'update', 'remove'], function(method) {
+        Meteor.default_server.method_handlers['/' + name + '/' + method] = function() {};
+      });
     });
-  });
+  }
   return collection;
 }
